eb831ba3cf778a643c77207602cf319c924545f8
ddmsh <129619224+ddmsh@users.noreply.github.com>
2024-12-22 23:09:02 +0900

    Update ether_list.ps1
    update list ether

--- ether_list.ps1
+++ ether_list.ps1
@@ -1,26 +1,13 @@
-# Get network adapters list
-$adapters = Get-NetAdapter -Physical | Where-Object { $_.MediaType -eq "802.3" }
-
-# Create custom adapter objects with Name, MAC Address, IP Address, Subnet Mask, Gateway, 1st DNS, and 2nd DNS properties
-$customAdapters = $adapters | ForEach-Object {
-    $adapterName = $_.Name
-    $macAddress = $_.MacAddress
-    $ipConfig = Get-NetIPConfiguration -InterfaceAlias $adapterName
-    $ipAddresses = ($ipConfig | Select-Object -ExpandProperty IPv4Address).IPAddress
-    $subnetMask = ($ipConfig | Select-Object -ExpandProperty IPv4Address).PrefixLength
-    $gateway = ($ipConfig | Select-Object -ExpandProperty IPv4DefaultGateway).NextHop
-    $dnsServers = $ipConfig.DNSServer.ServerAddresses
-
-    New-Object -TypeName PSObject -Property @{
-        Name = $adapterName
-        MAC = $macAddress
-        "IP Addresses" = ($ipAddresses -join ", ")
-        Subnet = $subnetMask
-        Gateway = $gateway
-        DNS = $dnsServers[0]
-        "2nd DNS" = $dnsServers[1]
-    }
-} | Select-Object Name, MAC, "IP Addresses", Subnet, Gateway, DNS, "2nd DNS"
-
-# Display adapter information in a table
-$customAdapters | Format-Table -AutoSize
+Get-NetAdapter -Physical | 
+    Where-Object { $_.MediaType -eq "802.3" } | 
+    ForEach-Object {
+        $config = Get-NetIPConfiguration -InterfaceAlias $_.Name
+        [PSCustomObject]@{
+            Name = $_.Name
+            MAC = $_.MacAddress
+            IP = ($config.IPv4Address.IPAddress -join ", ")
+            Subnet = $config.IPv4Address.PrefixLength
+            Gateway = $config.IPv4DefaultGateway.NextHop
+            DNS = ($config.DNSServer.ServerAddresses -join ", ")
+        }
+    } | Format-Table -AutoSize
